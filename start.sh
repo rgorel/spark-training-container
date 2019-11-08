48810faf8a348962b01e7e99c0997b9b1643997f
Roman Gorel <roman.gorel@crealytics.com>
2019-11-08 14:05:10 +0100

    fix start script

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 if [ ! -d datasets ]; then
   [ ! -f datasets.zip ] && wget https://s3.amazonaws.com/elephantscale-public/data/datasets.zip
   unzip datasets.zip
-  cd  datasets/text/twinkle/ && sh create-data-files.sh && cd -
+  cd  datasets/text/twinkle/ && ./create-data-files.sh && cd -
 fi
 
-ln -fs datasets /data
+ln -fs $(pwd)/datasets /data
 /spark/sbin/start-all.sh
 tail -f /dev/null
